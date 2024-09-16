3c3d93fe498ea912fff6ef34000f59700478cd40
Akhtar Rahat <rahatakhter4@gmail.com>
2024-09-17 04:32:51 +0600

    server created

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("hello");
+import express from "express";
+const app = express();
+
+const port = 3000;
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+    res.send(`Welcome to Personal-Finance`);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at port ${port}`);
+});
